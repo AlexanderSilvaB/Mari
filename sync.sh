8d1cd9333a89f16c33447df37fddb6347f684ad7
Alexander Silva Barbosa <alexander.lmx@outlook.com>
2018-10-15 13:07:02 -0300

    New sync options

--- sync.sh
+++ sync.sh
@@ -6,7 +6,6 @@ if [ "$#" -le 1 ]; then
     exit
 fi
 
-SSH_CMD="ssh -o ControlMaster=auto -o ControlPath=/tmp/control_%l_%h_%p_%r"
 robot=$1
 toolchain=$2
 sudo=false
@@ -17,10 +16,25 @@ home=true
 restart=false
 build=false
 configure=false
+uninstall=false
 clear=false
+pwd="nao"
+user="nao"
 
-for i in "$@" ; do
-    if [[ $i == "-sudo" ]] ; then
+nextIsPwd=false
+nextIsUser=false
+for i in "$@" ; do 
+    if [ $nextIsPwd == true ] ; then
+	pwd=$i
+	nextIsPwd=false
+    elif [ $nextIsUser == true ] ; then
+	user=$i
+	nextIsUser=false
+    elif [[ $i == "-pwd" ]] ; then
+        nextIsPwd=true
+    elif [[ $i == "-user" ]] ; then
+        nextIsUser=true
+    elif [[ $i == "-sudo" ]] ; then
         sudo=true
     elif [[ $i == "-clear" ]] ; then
         clear=true
@@ -36,6 +50,8 @@ for i in "$@" ; do
         restart=true
     elif [[ $i == "-build" ]] ; then
         build=true
+    elif [[ $i == "-uninstall" ]] ; then
+        uninstall=true
     elif [[ $i == "-configure" ]] ; then
         configure=true
     elif [[ $i == "-initial" ]] ; then
@@ -60,9 +76,16 @@ for i in "$@" ; do
     fi
 done
 
+SSH_CMD="sshpass -p $pwd ssh -o ControlMaster=auto -o ControlPath=/tmp/control_%l_%h_%p_%r"
+
 if [ $clear == true ]; then
     echo "Cleaning $toolchain"
     rm -rf src/build-$toolchain
+    exit
+fi
+
+if [ $uninstall == true ]; then
+    echo "Uninstalling from $robot"
     exit
 fi
 
@@ -82,7 +105,7 @@ if [ $ssh == true ]; then
     fi
 
     #cat ~/.ssh/id_rsa.pub | $SSH_CMD -l nao $robot "mkdir -p .ssh; sh -c 'cat >> .ssh/authorized_keys'; chmod 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys ; chmod g-w,o-w /home/nao"
-    ssh-copy-id nao@$robot
+    ssh-copy-id $user@$robot
 fi
 
 if [ $sudo == true ]; then
@@ -96,25 +119,25 @@ cp src/build-$toolchain/sdk/lib/libagent.so root/home/nao/naoqi/preferences/liba
 
 if [ $root == true ]; then
     echo "Sync root"
-    rsync -aburvP --rsync-path="sudo rsync" root/etc nao@$robot:/
+    sshpass -p $pwd rsync -aburvP --rsync-path="sudo rsync" root/etc $user@$robot:/
 fi
 
 if [ $home == true ]; then
     echo "Sync home"
-    rsync -aurvP root/home/nao/ nao@$robot:~/
+    sshpass -p $pwd rsync -aurvP root/home/nao/ $user@$robot:~/naoqi/
 fi
 
 if [ $services == true ]; then
     echo "Turning off services we do not use"
     # Add back in then delete because delete throws error if not installed
     #  * Networking connection manager replaced with runswiftwireless above
-    $SSH_CMD nao@$robot "sudo rc-update add connman boot"
-    $SSH_CMD nao@$robot "sudo rc-update del connman boot"
-    $SSH_CMD nao@$robot "sudo rc-update add qimessaging-json default"
-    $SSH_CMD nao@$robot "sudo rc-update del qimessaging-json default"
+    $SSH_CMD $user@$robot "sudo rc-update add connman boot"
+    $SSH_CMD $user@$robot "sudo rc-update del connman boot"
+    $SSH_CMD $user@$robot "sudo rc-update add qimessaging-json default"
+    $SSH_CMD $user@$robot "sudo rc-update del qimessaging-json default"
 fi
 
 if [ $restart == true ]; then
     echo "Restart Naoqi"
-    $SSH_CMD nao@$robot "sudo /etc/init.d/naoqi restart"
+    $SSH_CMD $user@$robot "sudo /etc/init.d/naoqi restart"
 fi
