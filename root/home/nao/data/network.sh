b66b7d27637f552422d339fe405f12aeb683408a
Alexander Silva Barbosa <alexander.lmx@outlook.com>
2018-11-07 11:03:39 -0200

    New network script

--- root/home/nao/data/network.sh
+++ root/home/nao/data/network.sh
@@ -4,9 +4,13 @@ team=47
 robot=01
 network=true
 ip=false
+server="10.0.0.100"
+mask="255.255.0.0"
 
 nextIsNumber=false
 nextIsTeam=false
+nextIsServer=false
+nextIsMask=false
 for i in "$@" ; do 
     if [ $nextIsNumber == true ] ; then
         robot=$i
@@ -14,6 +18,12 @@ for i in "$@" ; do
     elif [ $nextIsTeam == true ] ; then
         team=$i
 	    nextIsTeam=false
+    elif [ $nextIsServer == true ] ; then
+        server=$i
+	    nextIsServer=false
+    elif [ $nextIsMask == true ] ; then
+        mask=$i
+	    nextIsMask=false
     elif [[ $i == "-no-network" ]] ; then
         network=false
     elif [[ $i == "-set-ip" ]] ; then
@@ -22,12 +32,17 @@ for i in "$@" ; do
         nextIsNumber=true
     elif [[ $i == "-team" ]] ; then
         nextIsTeam=true
+    elif [[ $i == "-mask" ]] ; then
+        nextIsMask=true
+    elif [[ $i == "-server" ]] ; then
+        nextIsServer=true
     fi
 done
 
 IP="10.0.$team.$robot"
 
 if [ $network == true ]; then
+    echo "Setting netwrk profile"
     killall -q wpa_supplicant
     ifconfig wlan0 down
     sleep 2
@@ -37,9 +52,15 @@ if [ $network == true ]; then
 fi
 
 if [ $ip == true ]; then
-    ifconfig wlan0 "$IP" netmask 255.255.0.0
-    route add -net default gw "10.0.47.1"
+    echo "Setting IP = $IP"
+    echo "Setting netmask = $mask"
+    echo "Setting default gateway = $server"
+    ifconfig wlan0 "$IP" netmask "$mask"
+    route add -net default gw "$server"
 else
+    echo "Setting DHCP"
     killall -q dhclient
     dhclient -1 wlan0
 fi
+
+echo "Finished"
