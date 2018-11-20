de0a582e49207a86edbac262888469f58f030cd2
Alexander Silva Barbosa <alexander.lmx@outlook.com>
2018-11-20 11:22:01 -0200

    Remove video delete

--- src/Core/Utils/FrameWriter.cpp
+++ src/Core/Utils/FrameWriter.cpp
@@ -12,7 +12,7 @@ FrameWriter::FrameWriter(string fileName, int fps, Size size)
 {
     this->fileName = fileName;
     string cmd = "rm "+fileName;
-    system(cmd.c_str());
+    //system(cmd.c_str());
     writer.open(fileName, CV_FOURCC('M','J','P','G'), fps, size, true);
 }
 
